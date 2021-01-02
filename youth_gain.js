38fd97f6acc41fbc9d9f8df83ea8c5469d803ef1
cc0606 <74646647+cc0606@users.noreply.github.com>
2021-01-02 18:59:08 +0800

    Update youth_gain.js

--- youth_gain.js
+++ youth_gain.js
@@ -1,1 +1,2 @@
-https:\/\/ios\.baertt\.com\/v5\/task\/browse_(start|end)\.json url script-request-body youth_gain.js
+#中青浏览赚
+https:\/\/ios\.baertt\.com\/v5\/task\/browse_(start|end)\.json url script-request-body https://raw.githubusercontent.com/cc0606/QuantumultX/main/youth_gain.js
